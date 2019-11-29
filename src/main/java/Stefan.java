343fc82d88ea35021b1e7dc8bed9ce61a5a7979d
Michal <mkowalczewskisda@gmail.com>
2019-11-29 21:08:08 +0100

    sprawdzam gdzie bedzie commit

---
+++ src/main/java/Stefan.java
@@ -0,0 +1,15 @@
+public class Stefan {
+    String nazwisko;
+
+    public Stefan(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+}
